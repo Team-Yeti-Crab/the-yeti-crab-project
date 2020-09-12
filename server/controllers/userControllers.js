6085d352c928bc793cb6cf88c671891a83214c53
aaronyang024@gmail.com <aaronyang024@gmail.com>
2020-09-12 12:19:40 -0700

    update server to handle login requests + global err handler

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -9,3 +9,25 @@ const userController = {};
     //send confirmation back to client
   //if passwords don't match 
     //trigger error
+    //
+
+userController.logIn = (req, res, next) => {
+  // get username and password from req.body
+  let username = req.body.username;
+  let password = req.body.password;
+  // query from user where id is equal to username
+  const queryUser = 'SELECT * FROM users'
+  // check if username exits by querying the database
+  db.query(queryUser,(err,user) => {
+    // if err send to global err handler 
+    if (!user.username) {
+      return next({err}) 
+    } else {
+      // if password checks out send them to the main page
+      if (user[0].password === password) {
+        // if it is send them to main page
+        return next();
+      }
+    }
+  })
+}
