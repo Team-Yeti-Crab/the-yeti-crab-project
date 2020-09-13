d01d0b81480563c01e6aab298351bb500605780e
liameno16 <liameno16@gmail.com>
2020-09-12 18:49:12 -0700

    added login actions with thunk, axios, and helper actions to update store

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../constants/actiontypes';
+import axios from 'axios';
 
 // Create specific action functions that return object/dispatch
 export const loginPopup = (dispatch) => (
@@ -13,15 +14,40 @@ export const signupPopup = (dispatch) => (
   })
 );
 
-// Login will dispatch userInfo (object with username and password values) to reducer, saw docs that didnt user dispatch as param
+// LOGIN ACTIONS //
+// Login is a thunk that returns a function and let's store know of status of request at each point 
 export const login = (userInfo) => {
-  const action = {
-    type: actionTypes.LOGIN_AUTH,
-    payload: userInfo
+  return dispatch => {
+    dispatch(loginStarted());
+
+    // Post request to server, axios automatically parses response
+    axios.post('/api/login', {
+      username: userInfo.username,
+      password: userInfo.password
+    })
+    .then(res => dispatch(loginSuccess(res.data)))
+    .catch(err => dispatch(loginFailed(err)));
   }
-  dispatch(action);
 };
 
+// Login started lets store know request started
+export const loginStarted = () => ({
+  type: actionTypes.LOGIN_START
+});
+
+// login success dispatches upon successful completion of get request
+export const loginSuccess = (loginResponse) => ({
+  type: actionTypes.LOGIN_SUCCESS,
+  payload: {...loginResponse}
+});
+
+// Login failure dispatches if theres an error in the request
+export const loginFailed = (loginErr) => ({
+  type: actionTypes.LOGIN_FAILURE,
+  payload: {...loginErr}
+});
+
+// SIGNUP ACTIONS //
 // Signup will dispatch newUserinfo (also an object) to reducer
 export const signup = (newUserInfo) => {
   const action = {
--- client/constants/actiontypes.js
+++ client/constants/actiontypes.js
@@ -1,5 +1,10 @@
 // Need actions for login popup and signup popup
 export const LOGGING_IN = 'LOGGING_IN';
 export const SIGNING_UP = 'SIGNING_UP';
+// Login constants
 export const LOGIN_AUTH = 'LOGIN_AUTH';
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+// Signup constants
 export const SIGNUP_AUTH = 'SIGNUP_AUTH';
