148afcaa1b73d8dec8a879bd8af1ec14123125cc
davidemmolino <davidewebdev@gmail.com>
2020-09-14 16:32:42 -0700

    fixed some styling

--- client/components/HomePage/postCard.js
+++ client/components/HomePage/postCard.js
@@ -3,6 +3,9 @@ import React from 'react';
 const PostCard = props => {
   //assume we have title, pros, cons, user, time props
   const { title, pros, cons, date, user_id } = props
+  const images = ["../public/jesus_cropped.png", "../public/davide_cropped.png", "../public/aaron_cropped.png", "../public/liam_cropped.png", "../public/alex_cropped.png"]
+  const randomImages = images.map((el, i) => image[i])
+
 
   return (
 
@@ -10,7 +13,7 @@ const PostCard = props => {
     <div key={`${user_id}_${date}`} className="postCard">
 
       <div className="userPic">
-        <img src="../public/jesus_cropped.png" alt="profile pic"/>
+        <img src={`${randomImages}`} alt="profile pic"/>
       </div>
       <div className="postBody">
         <div className="postTitle">{title}</div>
