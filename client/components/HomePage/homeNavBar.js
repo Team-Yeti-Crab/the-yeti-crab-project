f581594d989050ef93a51813bcd6774705009793
aaronyang024@gmail.com <aaronyang024@gmail.com>
2020-09-14 16:20:05 -0700

    nav bar styling

--- client/components/HomePage/homeNavBar.js
+++ client/components/HomePage/homeNavBar.js
@@ -11,17 +11,16 @@ const HomeNavBar = props => {
       {/* Nav BAR LOGO */}
       {/* A cute yeti will be placed in the background of this container */}
       {/* On click should bring you back to home page / refresh home page */}
-      <div id="homeYetiLogo">Yeti Logo</div>
-
+      
+        <img src='../public/yeti_logo.png' alt='Cute Yeti logo' id='homeYetiLogo'></img>
+      
       {/* Use newPostToggle to render a button which will open up a post input modal. The input modal will have a 'post' button which will remove the modal */}
-
       <NewPostToggle 
         toggle={show=><button type="button" id="postButton" onClick={show}>+POST</button>}
         content={hide => <NewPostModal hide={hide} />}
       />
-
       {/* For now, profile pic will be set as a background image via CSS. Update this eventually to be a picture in a database? */}
-      <div id="profilePic">Profile Pic</div>
+      {/* <div id="profilePic">Profile Pic</div> */}
 
     </div>
   )
